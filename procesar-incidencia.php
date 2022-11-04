97bf7a3405e65a5b6e26d7117250b030c6369e01
Federico Gandaria <federicogandaria@gmail.com>
2022-11-04 20:45:11 -0300

    Creacion de formulario html con php

---
+++ procesar-incidencia.php
@@ -0,0 +1,10 @@
+<?php
+
+$usuario = $_POST["usuario"];
+$departamento = $_POST["depto"];
+$descripcion = $_POST["descripcion"];
+$passw = $_POST["contraseña"];
+
+echo "Nombre de usuario: ".$usuario."<br>","Departamento: " .$departamento."<br>","Descripcion: ".$descripcion."<br>","Contraseña: ".$passw."<br>";
+
+?>
